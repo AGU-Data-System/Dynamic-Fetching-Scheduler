e3384c6468776344489238331dfd02d9f266f021
Rodrigohcorreia <rodrigo.h.correia@gmail.com>
2024-03-01 16:15:34 +0000

    Rushed Commit (A lot of TODOs) for the other devs to keep working from there, because host dev had to go. - Changes by Sideghost

--- code/jvm/build.gradle.kts
+++ code/jvm/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.22"
 }
 
-group = "uagSystem"
+group = "dynamicFetchingScheduler"
 version = "0.0.1-SNAPSHOT"
 
 java {
--- code/jvm/settings.gradle.kts
+++ code/jvm/settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "UAGSystem"
+rootProject.name = "dynamicFetchingScheduler"
